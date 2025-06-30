dafdc947fcda54b767f7d0aff3ca9a5dcf5f3366
CJ Castillo <caytejohntims@gmail.com>
2025-06-30 11:50:05 +0800

    Initial commit

---
+++ roles/post-config/files/getburpcert.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+burp=$(find / -name burp*.jar 2>/dev/null | tail -1)
+/bin/bash -c "timeout 45 java -Djava.awt.headless=true -jar /usr/share/burpsuite/burpsuite.jar $burp < <(echo y) &" 
+sleep 40
+curl http://localhost:8080/cert -o /tmp/cacert.der
+exit
